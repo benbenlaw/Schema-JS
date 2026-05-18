d8992e4dd5006ffbff6b17b5d8fccb589da6b905
benbenlaw <benlawrenceben@hotmail.co.uk>
2026-05-18 16:34:48 +0100

    1.0.0

---
+++ src/main/java/com/benbenlaw/schemajs/SchemaJS.java
@@ -0,0 +1,31 @@
+package com.benbenlaw.schemajs;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.resources.Identifier;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.loading.FMLEnvironment;
+import net.neoforged.neoforge.client.event.RegisterPictureInPictureRenderersEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.AddServerReloadListenersEvent;
+import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(SchemaJS.MOD_ID)
+public class SchemaJS {
+
+    public static final String MOD_ID = "schemajs";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public SchemaJS(final IEventBus eventBus, final ModContainer modContainer) {
+
+    }
+
+}
+
