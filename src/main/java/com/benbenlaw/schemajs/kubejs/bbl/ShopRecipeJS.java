ad43193e12cf78e20253ccd8cef7870247ce1758
benbenlaw <benlawrenceben@hotmail.co.uk>
2026-07-03 15:20:15 +0100

    1.5.2

--- src/main/java/com/benbenlaw/schemajs/kubejs/bbl/ShopRecipeJS.java
+++ src/main/java/com/benbenlaw/schemajs/kubejs/bbl/ShopRecipeJS.java
@@ -15,7 +15,8 @@ public interface ShopRecipeJS {
     RecipeKey<Integer> SELL_PRICE = NumberComponent.INT.otherKey("sell_price");
 
     RecipeKey<String> TIER = StringComponent.OPTIONAL_STRING.otherKey("tier").functionNames("tier").defaultOptional();
+    RecipeKey<String> ORDER = StringComponent.OPTIONAL_STRING.otherKey("order").functionNames("order").defaultOptional();
 
-    RecipeSchema SCHEMA = new RecipeSchema(ITEM, BUY_PRICE, SELL_PRICE, TIER);
+    RecipeSchema SCHEMA = new RecipeSchema(ITEM, BUY_PRICE, SELL_PRICE, TIER, ORDER);
 
 }
